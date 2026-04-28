b30f7db4765d84c65727328fcc1d6c04545b9647
ntthuong25174600070 <ntthuong.25174600070@sv.uneti.edu.vn>
2026-04-28 13:23:06 +0700

    Delete 25174600070_Nguyễn Thị Thanh Hương directory

--- 25174600070_Nguyễn Thị Thanh Hương/b2.py
+++
@@ -1,8 +0,0 @@
-def fibonacci():
-    print("Dãy fibonacci (10 số đầu tiên)")
-    n1, n2 = 0, 1
-    for i in range(10):
-        print(n1, end=" ")
-        n1, n2 = n2, n1 + n2
-    print()
-fibonacci()
